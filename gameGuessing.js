be7b79fa389e59100843c4d36ab41b7386b4f6a7
Irene <irene.penuelas445@west-mec.org>
2016-11-09 14:19:36 -0800

    music added and background changes color

--- gameGuessing.js
+++ gameGuessing.js
@@ -5,29 +5,36 @@
 
 function generateRandom() {
         //Generating a number starting from 1 to 9
-//    var randColor = Math.floor(Math.random() * 9) + 1;
-      //Array of color options
+      //Array of color options:
     var color = ["BLUE", "CYAN", "GOLD", "GREEN", "MAGENTA", "ORANGE", "RED", "WHITE", "YELLOW"];
     var randColor = color[Math.floor(Math.random() * 9)];
     var submission = document.getElementById('submission').value.toUpperCase();
+    var results = document.getElementById('results');
+    var music = document.getElementById('music');
+
+
+
 
 //Checking the computer's choice
-alert(randColor);
+//alert(randColor);
 
     //Comparing randColor with the user's input
         //Winner winner chicken dinner
     if (submission == randColor) {
-        alert("You are a winner");
-        document.getElementById("html").style.backgroundColor = submission;
+        document.getElementById("theHtml").style.backgroundColor = submission;
+        results.innerHTML = "WINNER WINNER CHICKEN DINNER";
+        music.src = 'celebrateGoodTimes.mp3';
     }
         //Too high
     else if (submission > randColor) {
+        document.getElementById("theHtml").style.backgroundColor = "#ff4c4c";
         alert("The computer chose a color alphabetically lower than your guess. Too bad.");
-        document.getElementById("html").style.backgroundColor = "ff0000";
+        music.src = 'fail.mp3';
     }
         //Too low
     else if (submission < randColor) {
+        document.getElementById("theHtml").style.backgroundColor = "#ff4c4c";
         alert("The computer chose a color alphabetically higher than your guess. :( ");
-        document.getElementById("html").style.backgroundColor = "ff0000";
+        music.src = 'fail.mp3';
     }
 } //end function
